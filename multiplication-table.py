d587f79b5b6f1c9d3a8a8e4f48dbdafe851c3871
Eric Dennison <ericd@netdenizen.com>
2017-03-27 13:58:42 -0400

    Updated from Brython Server: 3/27/2017 1:58:41 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -28,3 +28,7 @@ for j in range(1, 10
  print("  ", j, end = '')
  print()
 print("-----------------------------------------")
+for i in range(1, 10):
+    print(i, "|", end = '')
+    for j in range(1, 10): 
+  

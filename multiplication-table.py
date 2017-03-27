e2dad253afbc80812e22a6f3c49b08cbe82d96d3
Eric Dennison <ericd@netdenizen.com>
2017-03-27 13:56:28 -0400

    Updated from Brython Server: 3/27/2017 1:56:27 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -21,3 +21,6 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+print("          Multiplication Table")
+
+print("  |", end = '')

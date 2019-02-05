5b3e48198668d3f4f9da4542f492b5d914e3a098
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-02-05 13:46:18 -0500

    Updated from Brython Server: 2/5/2019 1:46:17 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -24,7 +24,8 @@ Height of multiplication table: 8
 x = int(input("input"))
 y = int(input("input"))
 
-for row in x:
-    for column in y:
-        print(row)
+for row in range(1, x+1):
+    for column in range(1, y+1):
+        w= row*column
+        print(
     
